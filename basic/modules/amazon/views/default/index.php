5748402bc03bf3873810d01cf2bc4ed95199d8eb
AREON\RospopovS <Ser-19@ukr.net>
2017-02-03 18:10:31 +0200

    menu add started

--- basic/modules/amazon/views/default/index.php
+++ basic/modules/amazon/views/default/index.php
@@ -1,12 +1,7 @@
+<?php use yii\helpers\Url;?>
 <div class="amazon-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+    <ul>
+        <li><a href="<?=Url::toRoute('/amazon/hosts');?>">Hosts</a></li>
+        <li><a href="<?=Url::toRoute('/amazon/ean');?>">Eans</a></li>
+    </ul>
 </div>
--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -37,7 +37,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'Amazon', 'url' => ['/amazon/hosts']],
+            ['label' => 'Amazon', 'url' => ['/amazon']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
